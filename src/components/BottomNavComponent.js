89b2c40976f66127d503f408450f86bddfa5662d
Jamie Brawn <jameshenrybrawn@gmail.com>
2024-07-01 16:04:07 +0100

    creates initial app navigation

---
+++ src/components/BottomNavComponent.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { BottomNavigation, Text } from "react-native-paper";
+import Home from "../screens/Home";
+import RecordScreen from "../screens/RecordScreen";
+
+const TrailsRoute = () => <Home />;
+
+const RecordRoute = () => <RecordScreen />;
+
+export default BottomNavComponent = () => {
+  const [index, setIndex] = useState(0);
+  const [routes] = useState([
+    { key: "trails", title: "Trails", focusedIcon: "map" },
+    { key: "record", title: "Record", focusedIcon: "record-circle-outline" },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    trails: TrailsRoute,
+    record: RecordRoute,
+  });
+
+  return (
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
+  );
+};
---
+++ src/screens/RecordScreen.js
@@ -0,0 +1,32 @@
+import MapView from "react-native-maps";
+import { StyleSheet, Text, View } from "react-native";
+
+export default RecordScreen = () => {
+  return (
+    <View style={styles.container}>
+    {/*Render our MapView*/}
+      <MapView
+        style={styles.map}
+        //specify our coordinates.
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
+    </View>
+  );
+}
+//create our styling code:
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1, //the container will fill the whole screen.
+    justifyContent: "flex-end",
+    alignItems: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
