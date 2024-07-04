4a3bc6eaa14774ac2a9776b9c624253c46b2cb2a
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-04 10:56:46 +0100

    adds base URL for api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const walksApi = axios.create ({ baseUrl: "/api"});
+const walksApi = axios.create ({ baseUrl: "https://the-way-i-walk.onrender.com/api"});
 
 export const addWalk = (walkObject) => {
     return walksApi.post("/walks", walkObject).then(({ data }) => data)
