36b12958bc8f3301252a1916bcb6fb90ea260b0c
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-09 17:04:52 +0100

    conditional rendering on walk card

--- src/components/WalkCard.js
+++ src/components/WalkCard.js
@@ -1,14 +1,30 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Icon } from "react-native-paper";
 
 const WalkCard = ({ walk }) => {
   return (
     <View style={styles.card}>
-      <Text style={styles.title}>{walk.title}</Text>
-      <Text>{walk.description}</Text>
-      <Text>Distance: {walk.distance_km} km</Text>
-      <Text>Ascent: {walk.ascent} m</Text>
-      <Text>Rating: {walk.rating}</Text>
-      <Text>Difficulty: {walk.difficulty}</Text>
+      <Text variant="headlineMedium" style={{textAlign: "center"}} >{walk.title}</Text>
+      <Text variant="bodyLarge" style={{textAlign: "center"}} >{walk.description}</Text>
+      <View style={styles.centredMetrics}>
+                    <View style={styles.centredRow}>
+                        <Icon source="walk" size={24} />
+                        <Text variant="bodyMedium">
+                            Distance: {walk.distance_km}km
+                        </Text>
+                    </View>
+                    <View style={styles.centredRow}>
+                        <Icon source="slope-uphill" size={24} />
+                        <Text variant="bodyMedium"> Ascent: {walk.ascent.toFixed(2)}m</Text>
+                    </View>
+                </View>
+      {walk.rating && <Text>Rating: {walk.rating}</Text>}
+      {walk.difficulty && (
+        <View style={styles.centredRow}>
+          <Icon source="speedometer-slow" size={24} />
+          <Text> Difficulty: {walk.difficulty}/10</Text>
+        </View>
+        )}
     </View>
   );
 };
@@ -21,10 +37,17 @@ const styles = StyleSheet.create({
     borderRadius: 8,
     elevation: 1,
   },
-  title: {
-    fontSize: 18,
-    fontWeight: "bold",
-  },
+  centredRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+},
+centredMetrics: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+    paddingVertical: 5
+},
 });
 
 export default WalkCard;
