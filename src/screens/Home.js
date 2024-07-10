ddfbb5ad7ea545e778059655733dc1d13be5be86
EllieD33 <120728802+EllieD33@users.noreply.github.com>
2024-07-10 16:00:54 +0100

    replaces toggle slider with segmented buttons to improve accessibility

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -10,7 +10,7 @@ import {
   Animated,
   Button
 } from "react-native";
-import { IconButton, ActivityIndicator, Button as PaperButton, Switch } from "react-native-paper";
+import { IconButton, ActivityIndicator, Button as PaperButton, SegmentedButtons } from "react-native-paper";
 import MapView, { Marker, UrlTile, Callout } from "react-native-maps";
 import { useNavigation } from "@react-navigation/native";
 import * as Location from "expo-location";
@@ -128,10 +128,15 @@ export default Home = (refreshWalkList, setRefreshWalkList) => {
     <View style={styles.container}>
       <View style={styles.header}>
         <IconButton icon="tune-variant" onPress={toggleSlider} />
-        <View style={styles.toggleContainer}>
-          <Text>{isMapView ? "Map View" : "List View"}</Text>
-          <Switch value={isMapView} onValueChange={toggleView} />
-        </View>
+          <SegmentedButtons
+            style={styles.segmentedButtons}
+            value={isMapView ? 'map' : 'list'}
+            onValueChange={value => setIsMapView(value === 'map')}
+            buttons={[
+              { value: 'map', label: 'Map View' },
+              { value: 'list', label: 'List View' }
+            ]}
+          />
       </View>
       <View style={styles.contentContainer}>
       <Animated.View style={[styles.sliderView, { transform: [{ translateY: slideAnim }] }]}>
@@ -207,16 +212,10 @@ const styles = StyleSheet.create({
     zIndex: 2,
     flex: 1,
     flexDirection: "row",
-    justifyContent: "space-between",
     alignItems: "center",
     paddingHorizontal: 10,
-    paddingBottom: 10,
+    paddingVertical: 10,
     marginTop: Constants.statusBarHeight,
-  },
-  toggleContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
   },
   contentContainer: {
     zIndex:1,
@@ -264,6 +263,13 @@ const styles = StyleSheet.create({
     marginBottom: 5,
     color: '#888',
     textAlign: "center"
+  },
+  segmentedButtons: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center', 
+    height: 40, 
+    margin: 15,
   },
 });
 
